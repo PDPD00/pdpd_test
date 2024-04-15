3f31db2c58de5968cd2f090e15f41ab6b550e500
PiDianPiDian <neu_zzy@yeah.net>
2024-04-15 19:45:40 +0800

    测试版本号提交

---
+++ base/GetTime3.py
@@ -0,0 +1,10 @@
+import datetime
+
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
+print(datetime.datetime.now().strftime("%Y/%m/%d %X"))
